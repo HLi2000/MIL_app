cd86b149ef4a9b505b1ba110525633b1f5b80055
ShiyuW1214 <92521045+ShiyuW1214@users.noreply.github.com>
2022-01-03 16:14:37 +0000

    main changed skin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,18 @@ public class Main {
     //public static int flag = 0;
     public static void main(String[] args) throws Exception {
 
+
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        }catch(Exception e) {
+            System.out.println(e);
+        }
+
         login_full l = new login_full();
 
 /*
