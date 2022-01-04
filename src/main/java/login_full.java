4c8a456b2888936f31ceb07813ca6bbb41fc2b9b
ShiyuW1214 <92521045+ShiyuW1214@users.noreply.github.com>
2022-01-04 12:00:14 +0000

    delete useless comments

--- src/main/java/login_full.java
+++ src/main/java/login_full.java
@@ -104,21 +104,4 @@ public class login_full {
         });
     }
 
-/*
-    public void main(String[] args) {
-
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        }catch(Exception e) {
-            System.out.println(e);
-        }
-        new login_full(jFrame);
-    }
-}
-*/
 }
