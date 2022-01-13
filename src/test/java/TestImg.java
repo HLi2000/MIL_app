c21c23c4dd4c14e135d5d87a702f616e6e481ab3
“HLi2000” <lihao20001205@outlook.com>
2022-01-13 11:42:39 +0000

    UT

---
+++ src/test/java/TestImg.java
@@ -0,0 +1,29 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Test the Img object class
+ */
+
+public class TestImg {
+    @Test
+    public void TestImgSet(){
+        int id=1;
+        String modality="CT";
+        String region="Arm";
+        String patient_name="Peter";
+        String file_name="ct arm 2.png";
+        Img img=new Img();
+        img.setId(id);
+        img.setModality(modality);
+        img.setRegion(region);
+        img.setPatient_name(patient_name);
+        img.setFile_name(file_name);
+
+        Assert.assertEquals(img.getId(),id);
+        Assert.assertEquals(img.getModality(),modality);
+        Assert.assertEquals(img.getRegion(),region);
+        Assert.assertEquals(img.getPatient_name(),patient_name);
+        Assert.assertEquals(img.getFile_name(),file_name);
+    }
+}
