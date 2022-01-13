71e2ede58c9cd7df45ac513161556a21657cbe6d
“HLi2000” <lihao20001205@outlook.com>
2022-01-13 18:19:27 +0000

    Refine Client

--- src/main/java/Entities/Client.java
+++ src/main/java/Entities/Client.java
@@ -27,40 +27,11 @@ public class Client {
     public String login(User user) throws Exception{
         //set up connection
         URL myURL = new URL("https://mil-servlet.herokuapp.com/login");
-        HttpURLConnection conn = getHttpURLConnection(myURL);
 
-        //transform user to gson format
-        Gson gson = new Gson();
-        String jsonString = gson.toJson(user);
-
-        //set up the header
-        conn.setRequestMethod("POST");
-        conn.setRequestProperty("Accept", "application/json");
-        conn.setDoOutput(true);
-
-        //write the body of the request
-        try (OutputStream outputStream = conn.getOutputStream()) {
-            byte[] body = jsonString.getBytes(StandardCharsets.UTF_8);
-            outputStream.write(body, 0, body.length);
-        }
-        catch(Exception e){
-            return e.getMessage();
-        }
-
-        BufferedReader bufferedReader = new BufferedReader(new
-                InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
-        String inputLine;//responded text from servlet
-
-        // Read the body of the response
-        inputLine = bufferedReader.readLine();
-        bufferedReader.close();
-
-        return inputLine;
+        return getResult(user, myURL);
     }
 
-    private HttpURLConnection getHttpURLConnection(URL myURL) throws IOException {
-        return (HttpURLConnection) myURL.openConnection();
-    }
+
 
     /**
      * The register method post a User object to the servlet and get result as response
@@ -71,35 +42,8 @@ public class Client {
     public String register(User user) throws Exception{
         //set up connection
         URL myURL = new URL("https://mil-servlet.herokuapp.com/register");
-        HttpURLConnection conn = (HttpURLConnection) myURL.openConnection();
 
-        //transform user to gson format
-        Gson gson = new Gson();
-        String jsonString = gson.toJson(user);
-
-        //set up the header
-        conn.setRequestMethod("POST");
-        conn.setRequestProperty("Accept", "application/json");
-        conn.setDoOutput(true);
-
-        //write the body of the request
-        try (OutputStream outputStream = conn.getOutputStream()) {
-            byte[] body = jsonString.getBytes(StandardCharsets.UTF_8);
-            outputStream.write(body, 0, body.length);
-        }
-        catch(Exception e){
-            return e.getMessage();
-        }
-
-        BufferedReader bufferedReader = new BufferedReader(new
-                InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
-        String inputLine;//responded text from servlet
-
-        // Read the body of the response
-        inputLine = bufferedReader.readLine();
-        bufferedReader.close();
-
-        return inputLine;
+        return getResult(user, myURL);
     }
 
     /**
@@ -111,35 +55,8 @@ public class Client {
     public String delete(User user) throws Exception{
         //set up connection
         URL myURL = new URL("https://mil-servlet.herokuapp.com/delete");
-        HttpURLConnection conn = (HttpURLConnection) myURL.openConnection();
 
-        //transform user to gson format
-        Gson gson = new Gson();
-        String jsonString = gson.toJson(user);
-
-        //set up the header
-        conn.setRequestMethod("POST");
-        conn.setRequestProperty("Accept", "application/json");
-        conn.setDoOutput(true);
-
-        //write the body of the request
-        try (OutputStream outputStream = conn.getOutputStream()) {
-            byte[] body = jsonString.getBytes(StandardCharsets.UTF_8);
-            outputStream.write(body, 0, body.length);
-        }
-        catch(Exception e){
-            return e.getMessage();
-        }
-
-        BufferedReader bufferedReader = new BufferedReader(new
-                InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
-        String inputLine;//responded text from servlet
-
-        // Read the body of the response
-        inputLine = bufferedReader.readLine();
-        bufferedReader.close();
-
-        return inputLine;
+        return getResult(user, myURL);
     }
 
     /**
@@ -202,22 +119,8 @@ public class Client {
 
         // Set up the Connection
         URL myURL = new URL("https://mil-servlet.herokuapp.com/thumbnail");
-        HttpURLConnection conn = (HttpURLConnection) myURL.openConnection();
 
-        // Set up the header
-        conn.setRequestMethod("POST");
-        conn.setRequestProperty("Accept", "text/html");
-        conn.setRequestProperty("charset", "utf-8");
-        conn.setRequestProperty("Content-Length", Integer.toString(body.length));
-        conn.setDoOutput(true);
-
-        // Write the body of the request
-        try (OutputStream outputStream = conn.getOutputStream()) {
-            outputStream.write(body, 0, body.length);
-        }
-
-        // Read and return the body of the response
-        return conn.getInputStream();
+        return getInputStream(body, myURL);
     }
 
     /**
@@ -233,6 +136,59 @@ public class Client {
 
         // Set up the Connection
         URL myURL = new URL("https://mil-servlet.herokuapp.com/img");
+
+        return getInputStream(body, myURL);
+    }
+
+    /**
+     * Extracted method for post user info and get result resp
+     *
+     * @param user info
+     * @param myURL url
+     * @return result
+     */
+    private String getResult(User user, URL myURL) throws IOException {
+        //set up conn
+        HttpURLConnection conn = (HttpURLConnection) myURL.openConnection();
+
+        //transform user to gson format
+        Gson gson = new Gson();
+        String jsonString = gson.toJson(user);
+
+        //set up the header
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Accept", "application/json");
+        conn.setDoOutput(true);
+
+        //write the body of the request
+        try (OutputStream outputStream = conn.getOutputStream()) {
+            byte[] body = jsonString.getBytes(StandardCharsets.UTF_8);
+            outputStream.write(body, 0, body.length);
+        }
+        catch(Exception e){
+            return e.getMessage();
+        }
+
+        BufferedReader bufferedReader = new BufferedReader(new
+                InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
+        String inputLine;//responded text from servlet
+
+        // Read the body of the response
+        inputLine = bufferedReader.readLine();
+        bufferedReader.close();
+
+        return inputLine;
+    }
+
+    /**
+     * Extracted method for post req of filename and get img or thumbnail
+     *
+     * @param body req body
+     * @param myURL url
+     * @return InputStream of img or thumbnail
+     */
+    private InputStream getInputStream(byte[] body, URL myURL) throws IOException {
+        //set up conn
         HttpURLConnection conn = (HttpURLConnection) myURL.openConnection();
 
         // Set up the header
