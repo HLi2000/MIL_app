ffc8a07caf8ee848979431a9c59b2e1634d6fb9c
chenyurongrace <37578741+chenyurongrace@users.noreply.github.com>
2022-01-15 16:47:25 +0000

    Update Display.java
    Add comments

--- src/main/java/UI/Display.java
+++ src/main/java/UI/Display.java
@@ -3,58 +3,54 @@ package UI;
 import java.awt.*;
 import java.text.DecimalFormat;
 import javax.swing.*;
+
 /**
  * The UI.Zoom is used to let user see the image that they are interested at in its original size
  *
  * @author  Shiyu Wang, Yurong Chen
  * @since   2021-12-05
  */
+
 public class Display {
-    private String fileName;
-    private JFrame zoom = new JFrame();
-    private JPanel imagePanel = new JPanel();
 
     public Display(Image image, String file_name) throws Exception {
-
-        fileName = file_name;
+        JFrame zoom = new JFrame();
         zoom.setLocation(500, 20);
+
+        //get image size
         int w = image.getWidth(null);
         int h = image.getHeight(null);
 
-        System.out.println("w="+w);
-        System.out.println("h="+h);
+        //get screen size
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int screenHeight = screenSize.height;
+
         double scale;
-        /**
-         * Scale the image to prevent the image is too large or too small
-         * to view
-         */
-            if (w <= h) {
-                scale = 800*1000/h;
+        JPanel imagePanel = new JPanel();
 
-                System.out.println("scale="+new DecimalFormat("0.00").format(scale/1000));
-                zoom.setTitle(fileName+"   Scale Factor: " + new DecimalFormat("0.00").format(scale/1000));
-                Image scaled = image.getScaledInstance((int) ((w * scale)/1000), 800, Image.SCALE_DEFAULT);
-                System.out.println("scaled_w"+(w * scale)/1000);
+        // scale image
+        if (w <= h) {
+            scale = screenHeight*0.85*1000/h; // calculate scale factor
+            zoom.setTitle(file_name +"   Scale Factor: " + new DecimalFormat("0.00").format(scale/1000));
+            Image scaled = image.getScaledInstance((int) ((w * scale)/1000), 800, Image.SCALE_DEFAULT);
 
-                JLabel label = new JLabel(new ImageIcon(scaled));
-                imagePanel.add(label);
+            // display scaled image
+            JLabel label = new JLabel(new ImageIcon(scaled));
+            imagePanel.add(label);
+        } else {
+            scale = screenHeight*0.85*1000/w; //calculate scale factor
+            zoom.setTitle(file_name +"   Scale Factor: " + new DecimalFormat("0.00").format(scale/1000));
+            Image scaled = image.getScaledInstance(800, (int) ((h * scale)/1000), Image.SCALE_DEFAULT);
 
-            } else {
-                scale = 800*1000/w;
-                zoom.setTitle(fileName+"   Scale Factor: " + new DecimalFormat("0.00").format(scale/1000));
-                Image scaled = image.getScaledInstance(800, (int) ((h * scale)/1000), Image.SCALE_DEFAULT);
-                System.out.println("scaled_h"+(h * scale)/1000);
-                JLabel label = new JLabel(new ImageIcon(scaled));
-                imagePanel.add(label);
-            }
-
+            // display scaled image
+            JLabel label = new JLabel(new ImageIcon(scaled));
+            imagePanel.add(label);
+        }
 
         zoom.setLayout(new BorderLayout());
         zoom.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         zoom.setVisible(true);
-
         zoom.add(imagePanel);
         zoom.pack();
-
     }
 }
