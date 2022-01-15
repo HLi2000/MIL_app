dc902d6ce7c6a269757324dd7842e15411985f2f
ShiyuW1214 <92521045+ShiyuW1214@users.noreply.github.com>
2022-01-15 12:23:20 +0000

    delete some comments and add some new

--- src/main/java/UI/Display.java
+++ src/main/java/UI/Display.java
@@ -20,17 +20,19 @@ public class Display {
     public Display(Image image, String file_name) throws Exception {
 
         fileName = file_name;
-        //zoom.setTitle(fileName);
         zoom.setLocation(500, 20);
         int w = image.getWidth(null);
         int h = image.getHeight(null);
-        //DecimalFormat df = new DecimalFormat("0.00");
+
         System.out.println("w="+w);
         System.out.println("h="+h);
         double scale;
+        /**
+         * Scale the image to prevent the image is too large or too small
+         * to view
+         */
             if (w <= h) {
                 scale = 800*1000/h;
-                //Double result = df.format(scale);
 
                 System.out.println("scale="+new DecimalFormat("0.00").format(scale/1000));
                 zoom.setTitle(fileName+"   Scale Factor: " + new DecimalFormat("0.00").format(scale/1000));
@@ -42,9 +44,6 @@ public class Display {
 
             } else {
                 scale = 800*1000/w;
-                //System.out.println("scale="+scale);
-                //System.out.println("scale="+new DecimalFormat("0.00").format(scale/1000));
-                //zoom.setTitle(fileName+" Scaling factor: " + scale/1000);
                 zoom.setTitle(fileName+"   Scale Factor: " + new DecimalFormat("0.00").format(scale/1000));
                 Image scaled = image.getScaledInstance(800, (int) ((h * scale)/1000), Image.SCALE_DEFAULT);
                 System.out.println("scaled_h"+(h * scale)/1000);
@@ -52,54 +51,13 @@ public class Display {
                 imagePanel.add(label);
             }
 
-        //zoom.setSize(new Dimension(image.getWidth(null)));
 
         zoom.setLayout(new BorderLayout());
         zoom.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         zoom.setVisible(true);
-        //JPanel imagePanel = init(image);
-        //image.getScaledInstance(zoom.getWidth(), zoom.getHeight(), Image.SCALE_DEFAULT);
-        //JLabel label = new JLabel(new ImageIcon(scaled));
-        //imagePanel.add(label);
+
         zoom.add(imagePanel);
         zoom.pack();
-/*
-        zoom.addComponentListener(new ComponentAdapter() {
-            @Override
-            public void componentResized(ComponentEvent e) {
-                image.getScaledInstance(zoom.getWidth(), zoom.getHeight(), Image.SCALE_DEFAULT);
-                //JLabel label = new JLabel(new ImageIcon(newImage));
-                //imagePanel.add(label);
-            }
-        });
-
-
-    }
-/*
-    public JPanel init(Image image) throws Exception {
-        /*
-        int w = image.getWidth(null)*5;
-        int h = image.getHeight(null)*5;
-
-
-        zoom.addComponentListener(new ComponentAdapter() {
-            @Override
-            public void componentResized(ComponentEvent e) {
-                Image newImage = image.getScaledInstance(zoom.getWidth(), zoom.getHeight(), Image.SCALE_DEFAULT);
-                JLabel label = new JLabel(new ImageIcon(newImage));
-                imagePanel.add(label);
-            }
-        });
-        //Image newImage = image.getScaledInstance(w, h, Image.SCALE_DEFAULT);
-        //JLabel label = new JLabel(new ImageIcon(newImage));
-        //JPanel imagePanel = new JPanel();
-        //imagePanel.add(label);
-
-        return imagePanel;
-
-    }
-
-    */
 
     }
 }
